43097880f06723847a6656596fd893516bccc92f
Ishaq <isez9805@gmail.com>
2024-08-31 20:41:30 +0200

    #3: skeleton

---
+++ Lab1/Task3/Buffer.java
@@ -0,0 +1,19 @@
+public class Buffer {
+
+	public Buffer(int size) {
+		// TODO
+	}
+
+	void add(int i) {
+		// TODO
+	}
+
+	public int remove() {
+		// TODO
+		return -1;
+	}
+
+	public void close() {
+		// TODO
+	}
+}
