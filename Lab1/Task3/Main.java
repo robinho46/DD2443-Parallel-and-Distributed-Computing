96fe5e9a282e6d5d6cc0547f6ea0b3ae6cb90df8
Ishaq <isez9805@gmail.com>
2024-08-31 20:52:48 +0200

    Added Task 3 Template files

---
+++ Lab1/Task3/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+	public static class Producer implements Runnable {
+		public void run() {
+		}
+	}
+
+	public static class Consumer implements Runnable {
+		public void run() {
+		}
+	}
+
+	public static void main(String [] args) {
+	}
+}
