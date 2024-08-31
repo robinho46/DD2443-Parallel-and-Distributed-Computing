f2e9a1ecd859d7ac23452487b362e861e890050b
robinho46 <robin.yurt@hotmail.com>
2024-08-31 14:15:10 +0200

    Implementation for task A done, #1

--- Lab1/Task1/MainA.java
+++ Lab1/Task1/MainA.java
@@ -1,9 +1,32 @@
+import java.lang.Thread;
+
 public class MainA {
-	public static class Incrementer implements Runnable {
-		public void run() {
-		}
+    
+    static volatile int x;
+	
+    public static class Incrementer implements Runnable {       
+        public void run() {
+            for (int i = 0; i < 1000000; i++) {
+                   x += 1;
+               }   
+        }
 	}
 
 	public static void main(String [] args) {
-	}
+        int numOfThreads = 4;
+	    Thread[] threads = new Thread[numOfThreads];
+        for (int i = 0; i < numOfThreads; i++) {
+            threads[i] = new Thread(new Incrementer());
+            threads[i].start();
+        }
+
+        for(int i = 0; i < threads.length; i++){
+            try {
+                threads[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Final x: " + x);
+    }
 }
