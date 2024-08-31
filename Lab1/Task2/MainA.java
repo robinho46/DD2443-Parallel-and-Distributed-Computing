d561a73be91bb575c9e83e0b8b2b1f156566ad71
robinho46 <robin.yurt@hotmail.com>
2024-08-31 22:20:35 +0200

    Added skeleton for task 2, #2

---
+++ Lab1/Task2/MainA.java
@@ -0,0 +1,14 @@
+public class MainA {
+	public static class Incrementer implements Runnable {
+		public void run() {
+		}
+	}
+
+	public static class Printer implements Runnable {
+		public void run() {
+		}
+	}
+
+	public static void main(String [] args) {
+	}
+}
---
+++ Lab1/Task2/MainD.java
@@ -0,0 +1,14 @@
+public class MainD {
+	public static class Incrementer implements Runnable {
+		public void run() {
+		}
+	}
+
+	public static class Printer implements Runnable {
+		public void run() {
+		}
+	}
+
+	public static void main(String [] args) {
+	}
+}
