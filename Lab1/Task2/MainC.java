5babcccce852178b9159fcbff495606bd109d2c9
robinho46 <robin.yurt@hotmail.com>
2024-09-01 00:47:38 +0200

    Implementation of Task 2 A, B and C, #2

--- Lab1/Task2/MainC.java
+++ Lab1/Task2/MainC.java
@@ -1,14 +1,42 @@
 public class MainC {
-	public static class Incrementer implements Runnable {
+	private static int sharedInt = 0;
+    private static Object lock = new Object();
+    
+    public static class Incrementer implements Runnable {
 		public void run() {
-		}
+            for (int i = 0; i < 1000000; i++) {
+                sharedInt += 1;
+            }
+            synchronized(lock){
+                lock.notify();
+            }   
+        }
 	}
 
 	public static class Printer implements Runnable {
 		public void run() {
-		}
+          synchronized(lock){
+                try {
+                    lock.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println("Shared Int: " + sharedInt);
+        }
 	}
 
 	public static void main(String [] args) {
+        Thread t1 = new Thread(new Incrementer());
+        Thread t2 = new Thread(new Printer()); 
+        t1.start();
+        t2.start();
+ 
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 	}
 }
