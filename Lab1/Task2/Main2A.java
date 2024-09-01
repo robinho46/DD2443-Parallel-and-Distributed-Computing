866cdbb8682fdfc65dd62edda3b15487c06f69d1
robinho46 <robin.yurt@hotmail.com>
2024-09-01 16:40:07 +0200

    fixing indentation problem for Task A, #2

--- Lab1/Task2/Main2A.java
+++ Lab1/Task2/Main2A.java
@@ -1,21 +1,21 @@
 public class Main2A {
-	private static int sharedInt;
+    private static int sharedInt;
 
     public static class Incrementer implements Runnable {
-		public void run() {
+        public void run() {
             for (int i = 0; i < 1000000; i++) {
                 sharedInt += 1;
             }   		    
         }
-	}
+    }
 
-	public static class Printer implements Runnable {
-		public void run() {
-	        System.out.println("Shared Int: " + sharedInt);
+    public static class Printer implements Runnable {
+        public void run() {
+            System.out.println("Shared Int: " + sharedInt);
         }
-	}
+    }
 
-	public static void main(String [] args) {
+    public static void main(String [] args) {
         Thread t1 = new Thread(new Incrementer());
         t1.start();
         Thread t2 = new Thread(new Printer());
@@ -27,6 +27,5 @@ public class Main2A {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
     }
 }
