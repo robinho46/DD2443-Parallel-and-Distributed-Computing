170ceb9bb1941f3a7c6c1bed676cd34daefc079c
robinho46 <robin.yurt@hotmail.com>
2024-09-01 22:31:44 +0200

    Fixed trailing whitespaces

--- Lab1/Task3/Main.java
+++ Lab1/Task3/Main.java
@@ -1,13 +1,11 @@
 public class Main {
-
-	public static class Producer implements Runnable {
-		private final Buffer buffer;
-
+    public static class Producer implements Runnable {
+        private final Buffer buffer;
         public Producer(Buffer buffer) {
             this.buffer = buffer;
         }
 
-		public void run() {
+        public void run() {
             int size = 1000000;
             for(int i = 0; i < size; i++ ){
                 try {
@@ -18,47 +16,47 @@ public class Main {
             }
             try {
                 buffer.close();
-				Thread.currentThread().interrupt();
+                Thread.currentThread().interrupt();
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
         }
-	}
+    }
 
-	public static class Consumer implements Runnable {
-		private final Buffer buffer;
+    public static class Consumer implements Runnable {
+        private final Buffer buffer;
 
         public Consumer(Buffer buffer) {
             this.buffer = buffer;
         }
 
         public void run() {
-           try {
-			   while(true) {
-				   int k = buffer.remove();
-				   System.out.println(k);
-			   }
-		   } catch (InterruptedException e){
-			   Thread.currentThread().interrupt();
-		   } catch (IllegalStateException e){
-			   System.out.println("Consumed");
-		   }
+            try {
+                while(true) {
+                    int k = buffer.remove();
+                    System.out.println(k);
+                }
+            } catch (InterruptedException e){
+                Thread.currentThread().interrupt();
+            } catch (IllegalStateException e){
+                System.out.println("Consumed");
+            }
         }
-	}
+    }
 
-	public static void main(String [] args) {
-		int n = 100;
-		Buffer buffer = new Buffer(n);
-		Thread tp = new Thread(new Producer(buffer));
-		Thread tc = new Thread(new Consumer(buffer));
-		tp.start();
-		tc.start();
-
-		try {
-			tp.join();
-			tc.join();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
+    public static void main(String [] args) {
+        int n = 100;
+        Buffer buffer = new Buffer(n);
+        Thread tp = new Thread(new Producer(buffer));
+        Thread tc = new Thread(new Consumer(buffer));
+        tp.start();
+        tc.start();
+        
+        try {
+            tp.join();
+            tc.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
