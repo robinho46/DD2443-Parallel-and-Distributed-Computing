dce74c0790fa08c1de44cfb03db3ff525eb98cf9
robinho46 <robin.yurt@hotmail.com>
2024-09-08 19:44:31 +0200

    Added skeleton for Lab 2

---
+++ Lab2/task1/SequentialSort.java
@@ -0,0 +1,13 @@
+public class SequentialSort implements Sorter {
+
+        public SequentialSort() {
+        }
+
+        public void sort(int[] arr) {
+                // TODO Implement your sorting algorithm.
+        }
+
+        public int getThreads() {
+                return 1;
+        }
+}
