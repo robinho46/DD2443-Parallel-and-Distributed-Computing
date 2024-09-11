83010b614f4dd6e25d427aebd3c997a200c4f143
robinho46 <robin.yurt@hotmail.com>
2024-09-11 11:56:26 +0200

    changed sequential from int[] void

--- Lab2/src/SequentialSort.java
+++ Lab2/src/SequentialSort.java
@@ -1,52 +1,48 @@
 public class SequentialSort implements Sorter {
 
-    public SequentialSort() {
+        public SequentialSort() {}
     
-    }
-    
-    @Override
-    public int[] sort(int[] arr) {
-        if (arr.length <= 1) {
-            return arr; // Base case: if array has 1 or 0 elements, it's already sorted
-        }
-        int mid = arr.length / 2;
-        
-        int[] left = new int[mid];
-        int[] right = new int[arr.length - mid];
-        for (int i = 0; i < mid; i++) {
-            left[i] = arr[i]; 
-        }
-        for (int i = mid; i < arr.length; i++) {
-            right[i - mid] = arr[i];
+        @Override
+        public void sort(int[] arr) {
+                if (arr.length <= 1) {
+                    return; // Base case: if array has 1 or 0 elements, it's already sorted
+                }
+                int mid = arr.length / 2;
+
+                int[] left = new int[mid];
+                int[] right = new int[arr.length - mid];
+                for (int i = 0; i < mid; i++) {
+                    left[i] = arr[i]; 
+                }
+                for (int i = mid; i < arr.length; i++) {
+                    right[i - mid] = arr[i];
+                }
+
+                sort(left);
+                sort(right);
+                mergeSort(left, right, arr);
         }
 
-        int[] l = sort(left);
-        int[] r = sort(right);
-        return mergeSort(l, r, arr.length);
-    }
-    
-    public int[] mergeSort(int[] left, int[] right, int length){
-        int[] results = new int[length];
-        
-        int i = 0, j = 0, k = 0;
-        while (i < left.length && right.length > j) {
-            if(left[i] <= right[j]){
-                results[k++] = left[i++];
-            } else{
-                results[k++] = right[j++];
-            }
-        }
-       
-        while (left.length > i) {
-            results[k++] = left[i++];
-        }
-        while (right.length > j) {
-            results[k++] = right[j++];
-        }
-       return results;
-    }
+        public void mergeSort(int[] left, int[] right, int[] arr){
+                int i = 0, j = 0, k = 0;
+                while (i < left.length && right.length > j) {
+                    if(left[i] <= right[j]){
+                        arr[k++] = left[i++];
+                    } else{
+                        arr[k++] = right[j++];
+                    }
+                }
 
-    public int getThreads() {
+                while (left.length > i) {
+                    arr[k++] = left[i++];
+                }
+                while (right.length > j) {
+                    arr[k++] = right[j++];
+                }
+        }
+	
+	@Override
+        public int getThreads() {
             return 1; 
-    }
+        }
 }
