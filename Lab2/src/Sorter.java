aefbe0636cd5f0acb567851f2de0b682aca859c7
robinho46 <robin.yurt@hotmail.com>
2024-09-11 17:12:24 +0200

    fixed files

--- Lab2/src/Sorter.java
+++ Lab2/src/Sorter.java
@@ -4,7 +4,7 @@
 
 public interface Sorter {
     /** Sort 'arr' */
-    public int[] sort(int[] arr);
+    public void sort(int[] arr);
     /** Get the number of threads used by the implementation. */
     public int getThreads();
 }
