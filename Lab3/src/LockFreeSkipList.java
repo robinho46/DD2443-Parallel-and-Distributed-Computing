dbda3b16580eab746dc4453c7ad983aa658eca34
Ishaq <isez9805@gmail.com>
2024-10-27 20:08:03 +0100

    linpoint and validation

--- Lab3/src/LockFreeSkipList.java
+++ Lab3/src/LockFreeSkipList.java
@@ -1,201 +1,215 @@
 import java.util.concurrent.atomic.AtomicMarkableReference;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class LockFreeSkipList<T extends Comparable<T>> implements LockFreeSet<T> {
-        /* Number of levels */
-        private static final int MAX_LEVEL = 16;
+    /* Number of levels */
+    private static final int MAX_LEVEL = 16;
 
-        private final Node<T> head = new Node<T>();
-        private final Node<T> tail = new Node<T>();
+    private final Node<T> head = new Node<>();
+    private final Node<T> tail = new Node<>();
 
-        public LockFreeSkipList() {
-                for (int i = 0; i < head.next.length; i++) {
-                        head.next[i] = new AtomicMarkableReference<LockFreeSkipList.Node<T>>(tail, false);
-                }
+    private final ConcurrentLinkedQueue<Log.Entry> log = new ConcurrentLinkedQueue<>();
+
+    public LockFreeSkipList() {
+        for (int i = 0; i < head.next.length; i++) {
+            head.next[i] = new AtomicMarkableReference<>(tail, false);
         }
+    }
 
-        private static final class Node<T> {
-                private final T value;
-                private final AtomicMarkableReference<Node<T>>[] next;
-                private final int topLevel;
+    private static final class Node<T> {
+        private final T value;
+        private final AtomicMarkableReference<Node<T>>[] next;
+        private final int topLevel;
 
-                @SuppressWarnings("unchecked")
-                public Node() {
-                        value = null;
-                        next = (AtomicMarkableReference<Node<T>>[])new AtomicMarkableReference[MAX_LEVEL + 1];
-                        for (int i = 0; i < next.length; i++) {
-                                next[i] = new AtomicMarkableReference<Node<T>>(null, false);
-                        }
-                        topLevel = MAX_LEVEL;
-                }
-
-                @SuppressWarnings("unchecked")
-                public Node(T x, int height) {
-                        value = x;
-                        next = (AtomicMarkableReference<Node<T>>[])new AtomicMarkableReference[height + 1];
-                        for (int i = 0; i < next.length; i++) {
-                                next[i] = new AtomicMarkableReference<Node<T>>(null, false);
-                        }
-                        topLevel = height;
-                }
-        }
-
-        /* Returns a level between 0 to MAX_LEVEL,
-         * P[randomLevel() = x] = 1/2^(x+1), for x < MAX_LEVEL.
-         */
-        private static int randomLevel() {
-                int r = ThreadLocalRandom.current().nextInt();
-                int level = 0;
-                r &= (1 << MAX_LEVEL) - 1;
-                while ((r & 1) != 0) {
-                        r >>>= 1;
-                        level++;
-                }
-                return level;
+        @SuppressWarnings("unchecked")
+        public Node() {
+            value = null;
+            next = (AtomicMarkableReference<Node<T>>[]) new AtomicMarkableReference[MAX_LEVEL + 1];
+            for (int i = 0; i < next.length; i++) {
+                next[i] = new AtomicMarkableReference<>(null, false);
+            }
+            topLevel = MAX_LEVEL;
         }
 
         @SuppressWarnings("unchecked")
-        public boolean add(int threadId, T x) {
-                int topLevel = randomLevel();
-                int bottomLevel = 0;
-                Node<T>[] preds = (Node<T>[]) new Node[MAX_LEVEL + 1];
-                Node<T>[] succs = (Node<T>[]) new Node[MAX_LEVEL + 1];
-                while (true) {
-                        boolean found = find(x, preds, succs);
-                        if (found) {
-                                return false;
-                        } else {
-                                Node<T> newNode = new Node(x, topLevel);
-                                for (int level = bottomLevel; level <= topLevel; level++) {
-                                        Node<T> succ = succs[level];
-                                        newNode.next[level].set(succ, false);
-                                }
-                                Node<T> pred = preds[bottomLevel];
-                                Node<T> succ = succs[bottomLevel];
-                                if (!pred.next[bottomLevel].compareAndSet(succ, newNode, false, false)) {
-                                        continue;
-                                }
-                                for (int level = bottomLevel + 1; level <= topLevel; level++) {
-                                        while (true) {
-                                                pred = preds[level];
-                                                succ = succs[level];
-                                                if (pred.next[level].compareAndSet(succ, newNode, false, false))
-                                                        break;
-                                                find(x, preds, succs);
-                                        }
-                                }
-                                return true;
-                        }
-                }
+        public Node(T x, int height) {
+            value = x;
+            next = (AtomicMarkableReference<Node<T>>[]) new AtomicMarkableReference[height + 1];
+            for (int i = 0; i < next.length; i++) {
+                next[i] = new AtomicMarkableReference<>(null, false);
+            }
+            topLevel = height;
         }
+    }
 
-        @SuppressWarnings("unchecked")
-        public boolean remove(int threadId, T x) {
-                int bottomLevel = 0;
-                Node<T>[] preds = (Node<T>[]) new Node[MAX_LEVEL + 1];
-                Node<T>[] succs = (Node<T>[]) new Node[MAX_LEVEL + 1];
-                Node<T> succ;
-                while (true) {
-                        boolean found = find(x, preds, succs);
-                        if (!found) {
-                                return false;
-                        } else {
-                                Node<T> nodeToRemove = succs[bottomLevel];
-                                for (int level = nodeToRemove.topLevel; level >= bottomLevel+1; level --) {
-                                        boolean[] marked = {false};
-                                        succ = nodeToRemove.next[level].get(marked);
-                                        while (!marked[0]) {
-                                                nodeToRemove.next[level].compareAndSet(succ, succ, false, true);
-                                                succ = nodeToRemove.next[level].get(marked);
-                                        }
-                                }
-                                boolean[] marked = {false};
-                                succ = nodeToRemove.next[bottomLevel].get(marked);
-                                while (true) {
-                                        boolean iMarkedIt = nodeToRemove.next[bottomLevel].compareAndSet(succ, succ, false, true);
-                                        succ = succs[bottomLevel].next[bottomLevel].get(marked);
-                                        if (iMarkedIt) {
-                                                find(x, preds, succs);
-                                                return true;
-                                        } else if (marked[0]) {
-                                                return false;
-                                        }
-                                }
-                        }
-                }
+    /* Returns a level between 0 to MAX_LEVEL,
+     * P[randomLevel() = x] = 1/2^(x+1), for x < MAX_LEVEL.
+     */
+    private static int randomLevel() {
+        int r = ThreadLocalRandom.current().nextInt();
+        int level = 0;
+        r &= (1 << MAX_LEVEL) - 1;
+        while ((r & 1) != 0) {
+            r >>>= 1;
+            level++;
         }
+        return level;
+    }
 
-        public boolean contains(int threadId, T x) {
-                int bottomLevel = 0;
-                int key = x.hashCode();
+    @SuppressWarnings("unchecked")
+    public boolean add(int threadId, T x) {
+        int topLevel = randomLevel();
+        int bottomLevel = 0;
+        Node<T>[] preds = (Node<T>[]) new Node[MAX_LEVEL + 1];
+        Node<T>[] succs = (Node<T>[]) new Node[MAX_LEVEL + 1];
+        while (true) {
+            boolean found = find(x, preds, succs);
+            if (found) {
+                long linearizationTime = System.nanoTime(); // point - fail
+                log.add(new Log.Entry(Log.Method.ADD, x.hashCode(), false, linearizationTime));
+                return false;
+            } else {
+                Node<T> newNode = new Node<>(x, topLevel);
+                for (int level = bottomLevel; level <= topLevel; level++) {
+                    Node<T> succ = succs[level];
+                    newNode.next[level].set(succ, false);
+                }
+                Node<T> pred = preds[bottomLevel];
+                Node<T> succ = succs[bottomLevel];
+                if (!pred.next[bottomLevel].compareAndSet(succ, newNode, false, false)) {
+                    continue;
+                }
+                long linearizationTime = System.nanoTime(); // point - succ
+                log.add(new Log.Entry(Log.Method.ADD, x.hashCode(), true, linearizationTime));
+                for (int level = bottomLevel + 1; level <= topLevel; level++) {
+                    while (true) {
+                        pred = preds[level];
+                        succ = succs[level];
+                        if (pred.next[level].compareAndSet(succ, newNode, false, false)) {
+                            break;
+                        }
+                        find(x, preds, succs);
+                    }
+                }
+                return true;
+            }
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public boolean remove(int threadId, T x) {
+        int bottomLevel = 0;
+        Node<T>[] preds = (Node<T>[]) new Node[MAX_LEVEL + 1];
+        Node<T>[] succs = (Node<T>[]) new Node[MAX_LEVEL + 1];
+        Node<T> succ;
+        while (true) {
+            boolean found = find(x, preds, succs);
+            if (!found) {
+                long linearizationTime = System.nanoTime(); // point - fail
+                log.add(new Log.Entry(Log.Method.REMOVE, x.hashCode(), false, linearizationTime));
+                return false;
+            } else {
+                Node<T> nodeToRemove = succs[bottomLevel];
+                for (int level = nodeToRemove.topLevel; level >= bottomLevel + 1; level--) {
+                    boolean[] marked = {false};
+                    succ = nodeToRemove.next[level].get(marked);
+                    while (!marked[0]) {
+                        nodeToRemove.next[level].compareAndSet(succ, succ, false, true);
+                        succ = nodeToRemove.next[level].get(marked);
+                    }
+                }
                 boolean[] marked = {false};
-                Node<T> pred = head;
-                Node<T> curr = null;
-                Node<T> succ = null;
-                for (int level = MAX_LEVEL; level >= bottomLevel; level--) {
-                        curr = pred.next[level].getReference();
-                        while (true) {
-                                succ = curr.next[level].get(marked);
-                                while (marked[0]) {
-                                        curr = succ;
-                                        succ = curr.next[level].get(marked);
-                                }
-                                if (curr.value != null && x.compareTo(curr.value) < 0) {
-                                        pred = curr;
-                                        curr = succ;
-                                } else {
-                                        break;
-                                }
-                        }
-                }
-                return curr.value != null && x.compareTo(curr.value) == 0;
-        }
-
-        private boolean find(T x, Node<T>[] preds, Node<T>[] succs) {
-                int bottomLevel = 0;
-                boolean[] marked = {false};
-                boolean snip;
-                Node<T> pred = null;
-                Node<T> curr = null;
-                Node<T> succ = null;
-retry:
+                succ = nodeToRemove.next[bottomLevel].get(marked);
                 while (true) {
-                        pred = head;
-                        for (int level = MAX_LEVEL; level >= bottomLevel; level--) {
-                                curr = pred.next[level].getReference();
-                                while (true) {
-                                        succ = curr.next[level].get(marked);
-                                        while (marked[0]) {
-                                                snip = pred.next[level].compareAndSet(curr, succ, false, false);
-                                                if (!snip) continue retry;
-                                                curr = succ;
-                                                succ = curr.next[level].get(marked);
-                                        }
-                                        if (curr.value != null && x.compareTo(curr.value) < 0) {
-                                                pred = curr;
-                                                curr = succ;
-                                        } else {
-                                                break;
-                                        }
-                                }
-
-                                preds[level] = pred;
-                                succs[level] = curr;
+                    boolean iMarkedIt = nodeToRemove.next[bottomLevel].compareAndSet(succ, succ, false, true);
+                    long linearizationTime = System.nanoTime(); // point - succ/fail
+                    if (iMarkedIt) {
+                        log.add(new Log.Entry(Log.Method.REMOVE, x.hashCode(), true, linearizationTime));
+                        find(x, preds, succs);
+                        return true;
+                    } else {
+                        succ = nodeToRemove.next[bottomLevel].get(marked);
+                        if (marked[0]) { // point - special case
+                            log.add(new Log.Entry(Log.Method.REMOVE, x.hashCode(), false, linearizationTime));
+                            return false;
                         }
-                        return curr.value != null && x.compareTo(curr.value) == 0;
+                    }
                 }
+            }
         }
+    }
 
-        public Log.Entry[] getLog() {
-                // This should fetch the log from the skiplist.
-                return null;
-        }
-
-        public void reset() {
-                for (int i = 0; i < head.next.length; i++) {
-                        head.next[i] = new AtomicMarkableReference<LockFreeSkipList.Node<T>>(tail, false);
+    public boolean contains(int threadId, T x) {
+        int bottomLevel = 0;
+        boolean[] marked = {false};
+        Node<T> pred = head;
+        Node<T> curr = null;
+        Node<T> succ = null;
+        for (int level = MAX_LEVEL; level >= bottomLevel; level--) {
+            curr = pred.next[level].getReference();
+            while (true) {
+                succ = curr.next[level].get(marked);
+                while (marked[0]) {
+                    curr = succ;
+                    succ = curr.next[level].get(marked);
                 }
-                // TODO: Clear the log if you have one.
+                if (curr.value != null && x.compareTo(curr.value) > 0) {
+                    pred = curr;
+                    curr = succ;
+                } else {
+                    break;
+                }
+            }
         }
+        boolean result = curr.value != null && x.compareTo(curr.value) == 0;
+        long linearizationTime = System.nanoTime(); // point - res
+        log.add(new Log.Entry(Log.Method.CONTAINS, x.hashCode(), result, linearizationTime));
+        return result;
+    }
+
+    private boolean find(T x, Node<T>[] preds, Node<T>[] succs) {
+        int bottomLevel = 0;
+        boolean[] marked = {false};
+        boolean snip;
+        Node<T> pred = null;
+        Node<T> curr = null;
+        Node<T> succ = null;
+        retry:
+        while (true) {
+            pred = head;
+            for (int level = MAX_LEVEL; level >= bottomLevel; level--) {
+                curr = pred.next[level].getReference();
+                while (true) {
+                    succ = curr.next[level].get(marked);
+                    while (marked[0]) {
+                        snip = pred.next[level].compareAndSet(curr, succ, false, false);
+                        if (!snip) continue retry;
+                        curr = succ;
+                        succ = curr.next[level].get(marked);
+                    }
+                    if (curr.value != null && x.compareTo(curr.value) > 0) {
+                        pred = curr;
+                        curr = succ;
+                    } else {
+                        break;
+                    }
+                }
+                preds[level] = pred;
+                succs[level] = curr;
+            }
+            return curr.value != null && x.compareTo(curr.value) == 0;
+        }
+    }
+    
+    public Log.Entry[] getLog() {
+        return log.toArray(new Log.Entry[0]);
+    }
+
+    public void reset() {
+        for (int i = 0; i < head.next.length; i++) {
+            head.next[i] = new AtomicMarkableReference<>(tail, false);
+        }
+        log.clear();
+    }
 }
-
--- Lab3/src/Log.java
+++ Lab3/src/Log.java
@@ -1,29 +1,79 @@
+import java.util.Arrays;
+import java.util.Set;
+import java.util.HashSet;
+
 public class Log {
-        private Log() {
-                // Do not implement
+
+    private Log() {
+        // Do not implement
+    }
+
+    public static int validate(Log.Entry[] log) {
+        Arrays.sort(log, (e1, e2) -> Long.compare(e1.timestamp, e2.timestamp));
+
+        Set<Integer> hashSet = new HashSet<>();
+        int discrepancyCount = 0;
+
+        for (Log.Entry entry : log) {
+            boolean expectedReturn;
+
+            switch (entry.method) {
+                case ADD:
+                    expectedReturn = hashSet.add(entry.arg);
+                    if (expectedReturn != entry.ret) {
+                        discrepancyCount++;
+                        System.out.println("Discrepancy found in add: arg=" + entry.arg +
+                                           ", expected=" + expectedReturn +
+                                           ", actual=" + entry.ret +
+                                           ", timestamp=" + entry.timestamp);
+                    }
+                    break;
+
+                case REMOVE:
+                    expectedReturn = hashSet.remove(entry.arg);
+                    if (expectedReturn != entry.ret) {
+                        discrepancyCount++;
+                        System.out.println("Discrepancy found in remove: arg=" + entry.arg +
+                                           ", expected=" + expectedReturn +
+                                           ", actual=" + entry.ret +
+                                           ", timestamp=" + entry.timestamp);
+                    }
+                    break;
+
+                case CONTAINS:
+                    expectedReturn = hashSet.contains(entry.arg);
+                    if (expectedReturn != entry.ret) {
+                        discrepancyCount++;
+                        System.out.println("Discrepancy in contain: arg=" + entry.arg +
+                                           ", expected=" + expectedReturn +
+                                           ", actual=" + entry.ret +
+                                           ", timestamp=" + entry.timestamp);
+                    }
+                    break;
+
+                default:
+                    break;
+            }
         }
 
-        public static int validate(Log.Entry[] log) {
-                // Implement this.
-                // Should return the number of discrepancies in the log.
-                return -1;
-        }
+        return discrepancyCount;
+    }
 
-        // Log entry for linearization point.
-        public static class Entry {
-                public Method method;
-                public int arg;
-                public boolean ret;
-                public long timestamp;
-                public Entry(Method method, int arg, boolean ret, long timestamp) {
-                        this.method = method;
-                        this.arg = arg;
-                        this.ret = ret;
-                        this.timestamp = timestamp;
-                }
-        }
+    public static class Entry {
+        public Method method;
+        public int arg;
+        public boolean ret;
+        public long timestamp;
 
-        public static enum Method {
-                ADD, REMOVE, CONTAINS
+        public Entry(Method method, int arg, boolean ret, long timestamp) {
+            this.method = method;
+            this.arg = arg;
+            this.ret = ret;
+            this.timestamp = timestamp;
         }
+    }
+
+    public enum Method {
+        ADD, REMOVE, CONTAINS
+    }
 }
